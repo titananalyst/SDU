255cbf55935b6c0b7eb78f0c1db7912083e3b0fb
titananalyst <75612709+titananalyst@users.noreply.github.com>
2022-12-17 12:11:34 +0100

    Update model_2.py

--- PROG/Phase_2/model_2.py
+++ PROG/Phase_2/model_2.py
@@ -19,11 +19,13 @@ This module provided as material for Phase 1 of the exam project for DM562, DM85
 from __future__ import annotations # to use a class in type hints of its members
 from typing import Optional
 
+# This is a new Class
 class BasePatch:
+  """Represents a 'BasePatch' at the intersection of the riven row and column of the simulation grid."""
+
   def __init__(self:BasePatch, row:int, col:int):
     self._col = col
     self._row = row
-    self._obstactle = True
   
   def col(self:BasePatch)->int:
     """Returns the index of the column containing this patch."""
@@ -34,11 +36,24 @@ class BasePatch:
     return self._row
 
 
-class ObstaclePatch:
-  def __init__(self:ObstaclePatch):
-    self._is_obstacle = True
+# This is a new Class
+class ObstaclePatch(BasePatch):
+  """Represents a 'ObstaclePatch' at the intersection of the riven row and column of the simulation grid."""
 
-  def change(self):
+  def __init__(self:ObstaclePatch, row:int, col:int):
+    self._is_obstacle = True
+    self._col = col  # TODO: check if correctly gives col or ._col
+    self._row = row  # TODO: check if correctly takes row or ._row
+
+    def col(self:ObstaclePatch)->int:
+      """Returns the index of the column containing this patch."""
+    return self._col
+
+  def row(self:ObstaclePatch)->int:
+    """Returns the index of the row containing this patch."""
+    return self._row
+
+  def change(self:ObstaclePatch)->None:
     """This cell will no longer be an obstacle.
     It changes the status of the patch to a CellPatch
     
@@ -47,32 +62,38 @@ class ObstaclePatch:
     self._is_obstacle = False
     # TODO: maybe implement the patch changin to CellPatch or BasePatch
     
-
-  def is_obstacle(self):
+  def is_obstacle(self:ObstaclePatch)->bool:
     "Returns whether this is an obstacle patch."
+    return self._is_obstacle
 
 
+# This is the modified "Patch" class from Phase_1
 class CellPatch(BasePatch):
   """Represents a 'patch' at the intersection of the riven row and column of the simulation grid."""
   
-  def __init__(self:CellPatch):
-    super().__init__()
+  def __init__(self:CellPatch, row:int, col:int, toxicity:int):
     """    
     Parameters
     ----------
     row, col: int
       The index of the row and column containing this patch.
     """
-
+    self._col = col
+    self._row = row
+    self._toxicity = toxicity  # new Phase_2
     self._cell : Optional[Cell] = None
 
-  # def col(self:CellPatch)->int:
-  #   """Returns the index of the column containing this patch."""
-  #   return self._col
+  def col(self:CellPatch)->int:
+    """Returns the index of the column containing this patch."""
+    return self._col
 
-  # def row(self:CellPatch)->int:
-  #   """Returns the index of the row containing this patch."""
-  #   return self._row
+  def row(self:CellPatch)->int:
+    """Returns the index of the row containing this patch."""
+    return self._row
+
+  def toxicity(self:CellPatch)->int:  # new Phase_2
+    """Returns the toxicity level of this patch."""
+    return self._toxicity
   
   def has_cell(self:CellPatch)->bool:
     """Checks if the patch holds a cell."""
@@ -99,16 +120,29 @@ class CellPatch(BasePatch):
     """Returns a string representation of this patch."""
     return f"Patch({self.row()}, {self.col()})"
 
+
+# This is the modified "Cell" class from Phase_1
 class Cell:
   """Represents a cell in the simulation."""
 
-  def __init__(self:Cell,patch:CellPatch):
+  def __init__(self:Cell,patch:CellPatch,resistance_level:int):
     """    
     Parameters
     ----------
     patch: Patch
       The patch that will contain this cell (added automatically). The patch must be free.
     """
+    self._age_limit = 10  # new Phase_2 (int)
+    self._division_limit = 7  # new Phase_2 (int)
+    self._division_probability = 0.6  # new Phase_2 (float)
+    self._division_cooldown = 1 # new Phase_2 (int)
+    self._resistance_level = resistance_level  # new Phase_2 (int)
+    self._parent : Optional[Cell] = None  # new Phase_2 (cell input)
+    # TODO: not sure if parent should be written as argument in the constructor or not
+    #       since it is an optional input for the _parent attribute
+    self._generation = 0
+
+    # old from Phase_1
     self._patch = patch
     self._age = 0
     self._divisions = 0
@@ -117,6 +151,18 @@ class Cell:
     # inform patch that this cell is on it
     patch.put_cell(self)
   
+  def resistance(self:Cell)->int:  # new Phase_2
+    """Returns the resistance level of this cell."""
+    return self._resistance_level
+
+  def parent(self:Cell)->Optional[Cell]:  # new Phase_2
+    """Returns the parent of this cell."""
+    return self._parent
+
+  def generation(self:Cell)->int:  # new Phase_2
+    """Returns the generation level of this cell."""
+    return self._generation
+
   def patch(self:Cell)->CellPatch:
     """Returns the patch of this cell. If the cell is dead, it returns the patch where the cell died."""
     return self._patch
@@ -137,13 +183,15 @@ class Cell:
     """Returns whether this cell is alive."""
     return self._alive
 
-  def tick(self:Cell)->None:
+  def tick(self:Cell)->None:  # added generation cumulation per tick
     """Register with this cell that a tick in the simulation happened making the cell age.
     
     Precondition: the cell is alive."""
     assert self.is_alive(), "the cell must be alive."
     self._age = self._age + 1
     self._last_division = self._last_division + 1
+    self._generation = self._generation + 1  # new Phase_2
+    # TODO: Not sure if generation is correctly implemented here
 
   def die(self:Cell)->None:
     """This cell dies and is removed from its current patch.
@@ -162,3 +210,17 @@ class Cell:
     self._last_division = 0 # reset the counter from the last division
     self._divisions = self._divisions + 1 # updates the division count
     return Cell(patch)
+
+  def died_by_age(self:Cell)->bool:
+    # TODO: check age with age limit and if reached initial diying by age and increment statistics?
+    pass
+
+  def died_by_division(self:Cell)->bool:
+    # TODO: check division with division limit and if reached initial diying by age and increment statistics?
+    pass
+
+  def died_by_poisoning(self:Cell)->bool:
+    # TODO: check died by toxicity exceeded and if reached initial diying by age and increment statistics?
+    pass
+
+# TODO: add the last to changes from description.pdf and finish the other methods by diying
