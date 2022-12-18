db674e05dc4cb227f85b0995e52d586a6f4fd66b
titananalyst <75612709+titananalyst@users.noreply.github.com>
2022-12-18 18:15:08 +0100

    Update cell_sim_test.py

--- PROG/Phase_2/cell_sim_test.py
+++ PROG/Phase_2/cell_sim_test.py
@@ -8,12 +8,11 @@
 @Author  :   Jonas Keller
 '''
 
-from model import CellPatch, Cell
+from model import BasePatch, ObstaclePatch, CellPatch, Cell
 from visualiser import Visualiser
 import random
 from random import randint, choice
 from time import sleep
-
 import os
 
 # TODO: make a loader 
@@ -23,6 +22,9 @@ abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
 
+COLS = 0
+ROWS = 0
+GRID = []
 strGrid = 'grid_1.txt'
 
 def list_grids():
@@ -39,21 +41,7 @@ def loader(strGrid):
     return data
 print(loader(strGrid))
 
-def checker(input_grid):
-    # check if rows are bigger than 3
-    if len(input_grid) >= 3:
-        print("rows bigger than 3")
-    else:
-        print("there are not enough rows")
-        return False
-
-    # check if colums are bigger than 3
-    if all(len(element) >= 3 for element in input_grid):
-        print("cols bigger than 3")
-    else:
-        print("there are not enough cols")
-        return False
-
+def checker(input_grid, rows, cols):
     # Check if any element has a different length
     first_length = len(input_grid[0])
     if all(len(element) == first_length for element in input_grid):
@@ -61,9 +49,34 @@ def checker(input_grid):
     else:
         print("colums are not equal")
         return False
-    return True , input_grid
 
-print(checker(loader(strGrid)))
+    # check if rows are bigger than 3
+    if len(input_grid) >= 3:
+        print("rows bigger than 3")
+        rows = len(input_grid)
+    else:
+        print("there are not enough rows")
+        return False
 
+    # check if colums are bigger than 3
+    if all(len(element) >= 3 for element in input_grid):
+        print("cols bigger than 3")
+        cols = len(input_grid[0])
+    else:
+        print("there are not enough cols")
+        return False
+    return cols, rows, input_grid
+# print(checker(loader(strGrid), ROWS, COLS))
+COLS, ROWS, GRID = checker(loader(strGrid), ROWS, COLS)
 
+def initialize_grid():
+    global COLS, ROWS, GRID
+    # print(COLS, ROWS, GRID)
 
+    base_patches = [BasePatch(i, j) for i in range(ROWS) for j in range(COLS)]
+    
+    return base_patches
+
+patches = initialize_grid()
+for i in patches:
+    print(i.row(), i.col())
