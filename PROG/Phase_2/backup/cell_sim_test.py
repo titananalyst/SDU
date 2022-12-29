c754179903062d5413c75a03b919b38d5ed33649
titananalyst <75612709+titananalyst@users.noreply.github.com>
2022-12-29 22:24:31 +0100

    create backup

---
+++ PROG/Phase_2/backup/cell_sim_test.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+@File    :   cell_sim.py
+@Time    :   2022/11/08 01:28:29
+@Author  :   Simone Wolff Nielsen
+@Author  :   Mia Trabjerglund
+@Author  :   Jonas Keller
+'''
+
+from model import BasePatch, ObstaclePatch, CellPatch, Cell
+from visualiser import Visualiser
+import random
+from random import randint, choice
+from time import sleep
+import os
+
+# TODO: make a loader DONE
+# TODO: make a checker for the loaded grid validity DONE
+# TODO: make it possible to list dir to chose a different grid to load DONE
+# TODO: make function find neighbors of cells and implement the list of free neighbors
+#       in the same function by popping the obstacles and have cells out.
+# TODO: constaint of initialization population for row x col - obstacle patches
+# TODO: count the number of CellPatches in initialize_grid
+# TODO: import menu.py and run menu in cell_sim.py
+# TODO:
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+COLS = 0
+ROWS = 0
+GRID = []
+cells = []
+strGrid = 'grid_1.txt'
+
+def list_grids():
+    """Return a list of all grid files in the current directory."""
+    list_grids = []
+    for i in os.listdir(dname):
+        if i[-4:] == '.txt':
+            list_grids.append(i)
+    return list_grids
+# print(list_grids())
+
+def loader(strGrid):
+    """Returns a list with separated strings loaded from a chosen file
+    from list_grids."""
+    data = open(strGrid).read().split('\n')
+    return data
+# print(loader(strGrid))
+
+def checker(input_grid, rows, cols):
+    # TODO: try to implement doctests (look it up)
+    """Checks if the input is valid to initialize a grid
+    
+    >>> checker(input_grid, rows, cols)
+    Traceback (most recent call last):
+        ...
+    raise ValueError("colums are not equal")
+    ValueError: colums are not equal
+
+    """
+    # check that all lines contain % or int and not other letters
+    for element in input_grid:
+        for character in element:
+            if character != '%' and not isinstance(int(character), int):
+                raise ValueError("This line contains invalid characters: ", element)
+                # print("This line contains invalid characters: ", element)
+                # return False 
+    # TODO: also check special characters! now just numbers letters and "%"
+
+    # Check if any element has a different length
+    first_length = len(input_grid[0])
+    if all(len(element) == first_length for element in input_grid):
+        print("all columns equal")
+    else:
+        raise ValueError("colums are not equal")
+        # print("colums are not equal")
+        # return False
+
+    # check if rows are bigger than 3
+    if len(input_grid) >= 3:
+        print("rows bigger than 3")
+        rows = len(input_grid)
+    else:
+        raise ValueError("therea re not enough rows")
+        # print("there are not enough rows")
+        # return False
+
+    # check if colums are bigger than 3
+    if all(len(element) >= 3 for element in input_grid):
+        print("cols bigger than 3")
+        cols = len(input_grid[0])
+    else:
+        raise ValueError("there are not enough cols")
+        # print("there are not enough cols")
+        # return False
+    return cols, rows, input_grid
+# print(checker(loader(strGrid), ROWS, COLS))
+
+
+def initialize_grid():
+    """Return a list of BasePatches with the size of the input grid and
+    returns a list of Obstacle- and CellPatches.
+    The CellPatches have assigned the toxicity level to them."""
+    global COLS, ROWS, GRID
+    
+    base_patches = [BasePatch(i, j) for i in range(ROWS) for j in range(COLS)]
+    patches = []
+
+    for row in GRID:
+        for col in row:
+            base_patch = base_patches[0]
+
+            if col == '%':
+                temp = ObstaclePatch(base_patch.row(), base_patch.col())
+                patches.append(temp)
+
+            elif int(col) >= 0 and int(col) <= 9:
+                temp = CellPatch(base_patch.row(), base_patch.col(), int(col))
+                patches.append(temp)
+                
+            base_patches.pop(0)
+
+    return base_patches, patches
+
+def init_pop():
+    # TODO: initialize population random from CellPatches exlude ObstaclePatches
+    # patch = random.choice(patches)
+    # print(type(patch))
+
+    # cells.append(Cell(patches[30], 1))
+    pass
+
+class Grid():
+    def __init__(self):
+        # TODO: add the functions as methods, global variables as attributes
+        pass
+
+
+class Simulation():
+    def __init__(self):
+        
+
+
+        '''
+        Sim.tick(Cell)
+        #Deaths:
+        if self.died_by_age and Cell.died_by_division:
+            self._died_by_age +=1
+            self._died_by_division +=1
+            self._died_by_age_division += 1
+            if self.died_by_poisening:
+                self._died_by_poisening +=1
+                self._died_by_age_poisening +=1
+                self._died_by_division_poisening +=1
+                self._died_by_age_division_poisening +=1
+                self.die
+            self.die
+
+        elif self.died_by_age:
+            self._died_by_age +=1
+            if self.died_by_poisening:
+                self._died_by_poisening +=1
+                self._died_by_age_poisening +=1
+                self.die
+            self.die
+
+        elif self.died_by_division:
+            self._died_by_division +=1
+            if self.died_by_poisening:
+                self._died_by_poisening +=1
+                self._died_by_division_poisening +=1
+                self.die
+            self.die
+
+        elif self.died_by_poisening:
+            self._died_by_poisening +=1
+            self.die
+        '''
+
+
+
+
+
+
+# test functions
+if __name__ == "__main__":
+    # import doctest
+    # doctest.testmod()
+    COLS, ROWS, GRID = checker(loader(strGrid), ROWS, COLS)
+    base_patches, patches = initialize_grid()
+
+    vis = Visualiser(patches, ROWS, COLS, grid_lines=True)
+    vis.wait_close()
---
+++ PROG/Phase_2/backup/menu.py
@@ -0,0 +1,225 @@
+"menu"
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+@File    :   cell_sim.py
+@Time    :   2022/11/08 01:28:29
+@Author  :   Simone Wolff Nielsen
+@Author  :   Mia Trabjerglund
+@Author  :   Jonas Keller
+'''
+
+from cell_sim import Grid, Simulation
+from time import sleep
+from sys import exit
+
+grid = Grid() # sets grid to the whole class Grid from cell_sim
+sim = Simulation()
+sim_status = "Default"
+vis_status = "Enabled"
+
+def grid_menu(grid:str, GRID)->bool:
+    '''
+    this menue has to be completed before the next one is availeble. 
+
+    '''
+    grid = input("What is the name of the grid you want to run?").strip()
+    if GRID(grid):
+        choose_number_of_cells()
+    else:
+        print("make an valid grid, for guide see the raport. When you have a valid grid run the program again")
+        print("Bye!")
+        exit()
+
+def choose_number_of_cells(number:int):
+    '''
+    this should be running after the grid menue has come true
+    '''
+    print("choose an number of cell-population between 1 and", grid.total_num_cell_patc )
+    number = int(input("coose an number of cell-population here: "))
+    if 1 <= number >= grid.total_num_cell_patch:
+        sim.board.init_pop = number
+        print_menu()
+    else:
+        print("this number is not valied")
+        number = int(input('''do you want to choose a valid number of cell-population or exit?
+        1) try again.
+        anny other input) exit. 
+        choose here: '''))
+        if number == 1:
+            choose_number_of_cells()
+        else:
+            print("Bye!")
+            exit()
+
+
+def print_menu():
+    '''
+    this print out the display of the menu, 
+    where all the print-statements exicute one line at a time. 
+    '''
+    print("\nMenu:")
+    print(31 * "-")
+    print("1: Display configuration")
+    print("2: Setup")
+    print("3: Run simulation")
+    print("4: Reset to default setup")
+    print("5: Quit")
+    print()
+    menu_choice = int(input("Type in a number (1-5): "))
+
+    if menu_choice == 1:
+        if sim.visualisation == False:
+            vis_status = "Disabled"
+        '''
+        this print out the display confuration menu, 
+        with the parametres there has been chosen.
+        '''
+        print("\n" + 31 * "-")
+        print("{:<22} {}".format("Parameter", sim_status))
+        print(31 * "-")
+        print("{:<22} {}".format("Initial population", sim.board.init_pop))
+        print("{:<22} {}".format("Age limit", sim.board.age_lim))
+        print("{:<22} {}".format("Division limit", sim.board.div_lim))
+        print("{:<22} {}".format("Division probability", sim.board.prob))
+        print("{:<22} {}".format("Division cooldown", sim.board.cooldown))
+        print("{:<22} {}".format("Time limit", sim.max_ticks))
+        print("{:<22} {}\n".format("Visualisation", vis_status))
+        print_menu()
+
+
+    elif menu_choice == 2:
+            '''
+            this is a go through of the setting in Qick menu. 
+            exaple 1:
+            age_input is set to be an integere with the code int(). 
+            Inside int there is a code input() witch make the user avalible to make its own choice,
+            but that is restricted by the int().
+            That is why there is a messege inside the input() that the user will get. 
+            if the user select correctly the first if statement will be exicuted. 
+            inside that if there i a logical statement that checks if the user has made an valid choice.
+            if not the else statement will print, the "Default" are enabled and then return the user to the menue.
+            '''
+       
+            sim_status = "Setup"
+            
+            age_input = int(input("Enter a age limit for a cell (min 1): "))
+            if not age_input < 1:
+                sim.board.age_lim = age_input
+            else:
+                print("\nPlease enter a integer above 0")
+                print("Try again!\n")
+                print("default values are enabled")
+                sim_status = "Default"
+                print_menu()
+                
+
+            div_input = int(input("Enter number of max divisions of a cell (min 1): "))
+            if not div_input < 1:
+                sim.board.div_lim = div_input
+            else:
+                print("\nPlease enter a integer above 0")
+                print("Try again!\n")
+                print("default values are enabled")
+                sim_status = "Default"
+                sim.board.age_lim = 10
+                print_menu()
+                
+
+            prob_input = float(input("Enter probability for a cell to divide (between 0 and 1): "))
+            if prob_input >= 0 and prob_input <= 1:
+                sim.board.prob = prob_input
+            else:
+                print("\nPlease enter a float between 0 and 1")
+                print("Try again!\n")
+                print("default values are enabled")
+                sim_status = "Default"
+                sim.board.age_lim = 10
+                sim.board.div_lim = 2
+                print_menu()
+                
+
+            cooldown_input = int(input("Enter ticks of cooldown for one cell to divide (not negative): "))
+            if not cooldown_input < 0:
+                sim.board.cooldown = cooldown_input
+            else:
+                print("\nPlease enter a non negative number")
+                print("Try again!\n")
+                print("default values are enabled")
+                sim_status = "Default"
+                sim.board.age_lim = 10
+                sim.board.div_lim = 2
+                sim.board.prob = 0.2
+                print_menu()
+                
+
+            ticks_input = int(input("Enter number of ticks for the duration of the simulation (min 1): "))
+            if ticks_input >= 1:    
+                sim.max_ticks = ticks_input
+            else:
+                print("\nPlease enter a number above 0")
+                print("Try again!\n")
+                print("default values are enabled")
+                sim_status = "Default"
+                sim.board.age_lim = 10
+                sim.board.div_lim = 2
+                sim.board.prob = 0.2
+                sim.board.cooldown = 2
+                print_menu()
+                
+
+            vis_input = int(input("Enter 1 or 0 [ENABLE | DISABLE] the visualisation: "))
+            if vis_input == 0:
+                sim.visualisation = False
+            elif vis_input == 1:
+                sim.visualisation = True
+            else:
+                print("\nVisualisation configuration has not changed!")
+                print("Please chose between 1 and 0 [ENABLE | DISABLE]")
+                print("Try again!\n")
+                
+
+            print("Changed to advanced setup.")
+            print_menu()
+
+
+    elif menu_choice == 3:
+        '''
+        this starts the simulation that has been called from the cell_sim.py
+        '''
+        sim.start()
+        print_menu()
+
+        
+    elif menu_choice == 4:
+        '''
+        this reset alle tha parametres to the pre-chosen default values.
+        '''
+        sim_status = "Default"
+        sim.board.age_lim = 10
+        sim.board.div_lim = 2
+        sim.board.prob = 0.2
+        sim.board.cooldown = 2
+        sim.max_ticks = 100
+        sim.visualisation = True
+
+        print("Settings reset to default settings.\n")
+        print_menu()
+
+    
+    elif menu_choice == 5:
+        '''
+        quit the program with the imported quit function. 
+        '''
+        print('Bye!')
+        exit()
+
+    
+    else:
+        print('\nEnter a valid option!')
+
+
+# except ValueError:
+#     print('\n--- fatal ERROR!--- | Try again and enter just integer numbers!\n')
+#     sleep(1)
+#     print_menu()
